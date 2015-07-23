04edffbda373e9ae041b684df382da82d406297c
Ianto Lin Xi <iantolinxi@gmail.com>
2015-07-22 21:24:54 -0700

    removed ugly title bar

--- mobile/src/main/java/com/iantoxi/jetlagtrainer/MainActivity.java
+++ mobile/src/main/java/com/iantoxi/jetlagtrainer/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +13,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
     }
 
     @Override
