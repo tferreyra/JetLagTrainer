20338cac337fdd21e35545778d9fee9e4b40fe73
Ianto Lin Xi <iantolinxi@gmail.com>
2015-07-23 02:24:12 -0700

    Main activity started.

--- mobile/src/main/java/com/iantoxi/jetlagtrainer/MainActivity.java
+++ mobile/src/main/java/com/iantoxi/jetlagtrainer/MainActivity.java
@@ -1,9 +1,11 @@
 package com.iantoxi.jetlagtrainer;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 
 
@@ -12,8 +14,8 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
     }
 
     @Override
@@ -37,4 +39,7 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }
