4885ffd2deb7a33d28a32f5ebe149292c37f471e
Ianto Lin Xi <iantolinxi@gmail.com>
2015-07-25 03:13:44 -0700

    New activities, representing major scaffolding of project. Transitions between activities, sqlite database for recording history or evaluations.

--- mobile/src/main/java/com/iantoxi/jetlagtrainer/ScheduleActivity.java
+++ mobile/src/main/java/com/iantoxi/jetlagtrainer/ScheduleActivity.java
@@ -1,23 +1,21 @@
 package com.iantoxi.jetlagtrainer;
 
 import android.app.Activity;
-import android.app.ActivityOptions;
-import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.transition.Slide;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.Window;
 
 
-public class InputSelectionActivity extends Activity {
+public class ScheduleActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_input_selection);
+        setContentView(R.layout.activity_schedule);
         Slide slide = new Slide();
         slide.excludeTarget(android.R.id.statusBarBackground, true);
         slide.excludeTarget(android.R.id.navigationBarBackground, true);
@@ -28,7 +26,7 @@ public class InputSelectionActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_input_selection, menu);
+        getMenuInflater().inflate(R.menu.menu_schedule, menu);
         return true;
     }
 
@@ -45,10 +43,5 @@ public class InputSelectionActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void generateSchedule(View view) {
-        Intent intent = new Intent(this, ScheduleActivity.class);
-        startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
     }
 }
