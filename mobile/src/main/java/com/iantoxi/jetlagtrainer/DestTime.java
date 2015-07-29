66184514f19818e1b95eecbd23fcf9a4acb2bd65
Tatiana Ferreyra <tatianaferreyra@tatianas-mbp.gateway.pace.com>
2015-07-29 00:56:10 -0700

    started origin and dest time pickers

---
+++ mobile/src/main/java/com/iantoxi/jetlagtrainer/DestTime.java
@@ -0,0 +1,69 @@
+package com.iantoxi.jetlagtrainer;
+
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.transition.Slide;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.EditText;
+
+/**
+ * Created by tatianaferreyra on 7/29/15.
+ */
+public class DestTime extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_destination_time);
+        Slide slide = new Slide();
+        slide.excludeTarget(android.R.id.statusBarBackground, true);
+        slide.excludeTarget(android.R.id.navigationBarBackground, true);
+        getWindow().setEnterTransition(slide);
+        getWindow().setExitTransition(slide);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_input_selection, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void onStart() {
+        super.onStart();
+        EditText txtDate = (EditText) findViewById(R.id.editDestTime);
+        txtDate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    DateDialog dialog = new DateDialog(v);
+                    FragmentTransaction ft = getFragmentManager().beginTransaction();
+                    dialog.show(ft, "TimePicker");
+
+                }
+            }
+        });
+
+    }
+
+}
