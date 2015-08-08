9cd3e7838125eb84f44ac19ea917d414cb11d139
David Ju <davidju@berkeley.edu>
2015-08-08 12:59:14 -0700

    data transfer from mobile to wear should be working, I think

--- wear/src/main/java/com/iantoxi/jetlagtrainer/MainActivity.java
+++ wear/src/main/java/com/iantoxi/jetlagtrainer/MainActivity.java
@@ -29,14 +29,14 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        setContentView(R.layout.round_activity_main);
+        /*final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextView = (TextView) stub.findViewById(R.id.text);
             }
-        });
+        });*/
 
         SensorEventListener sensorListener = new SensorEventListener() {
             @Override
