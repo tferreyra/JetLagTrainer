fe4026896096bf714026612989d3b1401552d6fc
David Ju <davidju@berkeley.edu>
2015-08-12 06:12:24 -0700

    changed sleep-sensing logic so that there must be an interval of at least ten minutes between notifications

--- wear/src/main/java/com/iantoxi/jetlagtrainer/MainActivity.java
+++ wear/src/main/java/com/iantoxi/jetlagtrainer/MainActivity.java
@@ -7,6 +7,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.wearable.view.WatchViewStub;
 import android.view.View;
 import android.widget.Button;
@@ -18,27 +19,28 @@ import java.util.TimerTask;
 
 public class MainActivity extends Activity {
 
-    private TextView mTextView;
     private SensorManager sensorManager;
     private Sensor heartRateSensor, accelerationSensor;
     private Timer timer;
     private TimerTask notificationTask;
     private boolean isNotificationTaskRunning = false;
-    private int heartRateThreshold = 55, delay = 60000;
+    private int heartRateThreshold = 55;
+    private long delay = 60000, startTime = 0;
 
-    private boolean heartRate = false, acceleration = false;
+    private boolean heartRate = false, acceleration = false, wasNotificationTriggered = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.round_activity_main);
 
+
         SensorEventListener sensorListener = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent event) {
                 if (event.sensor.getType() == Sensor.TYPE_HEART_RATE) {
                     int currentHeartRate = (int) event.values[0];
-                    if (currentHeartRate <= heartRateThreshold) { // threshold may not/probably isn't accurate, can figure out what it should be later
+                    if (currentHeartRate <= heartRateThreshold && hasTimePassed()) { // threshold may not/probably isn't accurate, can figure out what it should be later
                         heartRate = true;
                         triggerNotification();
                     } else {
@@ -59,7 +61,7 @@ public class MainActivity extends Activity {
                 float x = event.values[0];
                 float y = event.values[1];
                 float z = event.values[2];
-                if (x == 0.5 && y == 0.5 && z == 0.5) {
+                if (x == 0.5 && y == 0.5 && z == 0.5 && hasTimePassed()) {
                     acceleration = true;
                     triggerNotification();
                 } else {
@@ -101,11 +103,37 @@ public class MainActivity extends Activity {
                 }
             };
             timer.schedule(notificationTask, delay);
+            startTime = System.currentTimeMillis();
             isNotificationTaskRunning = true;
+
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    if (isNotificationTaskRunning) {
+                        wasNotificationTriggered = true;
+                        isNotificationTaskRunning = false;
+                    } else {
+                        wasNotificationTriggered = false;
+                    }
+                }
+            }, delay);
+
+
         } else if ((!acceleration || !heartRate) && isNotificationTaskRunning) {
             notificationTask.cancel();
+            startTime = 0;
             isNotificationTaskRunning = false;
         }
+    }
+
+
+    private boolean hasTimePassed() {
+        long interval = 1000 * 60 * 11; //interval of ten minutes between notifications
+        if (System.currentTimeMillis() - startTime >= interval)
+            return true;
+        else
+            return false;
     }
 
 }
