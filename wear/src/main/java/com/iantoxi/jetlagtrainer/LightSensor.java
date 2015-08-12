168f7e30772143ac850181c2032bb2c2c5a375ec
David Ju <davidju@berkeley.edu>
2015-08-12 07:09:41 -0700

    slight corrections to light sensor detection logic

--- wear/src/main/java/com/iantoxi/jetlagtrainer/LightSensor.java
+++ wear/src/main/java/com/iantoxi/jetlagtrainer/LightSensor.java
@@ -11,6 +11,7 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.wearable.view.WatchViewStub;
 import android.view.View;
 import android.widget.Button;
@@ -34,41 +35,13 @@ public class LightSensor extends Activity {
        Logic here is that if there is significant change in brightness (from light to dark or vice versa)
        wear will notify watch to check whether user is supposed to be getting light or staying in the dark
        https://en.wikipedia.org/wiki/Lux */
-    private int lightnessThrehold = 7000, darknessThreshold = 10, taskDelay = 10;
+    private int lightnessThrehold = 7000, darknessThreshold = 10;
+    private long taskDelay = 1000 * 60 * 5;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.light_listener_notifier);
         timer = new Timer();
-
-        // Uncomment below to turn center logo into button for testing.
-/*        ImageView button = (ImageView) findViewById(R.id.logo);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LightSensor.this, SendServiceToMobile.class);
-                intent.putExtra("message", "sleeping");
-                startService(intent);
-            }
-        });
-        RelativeLayout relative1 = (RelativeLayout) findViewById(R.id.top);
-        relative1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LightSensor.this, SendServiceToMobile.class);
-                intent.putExtra("message", "light");
-                startService(intent);
-            }
-        });
-        RelativeLayout relative2 = (RelativeLayout) findViewById(R.id.bottom);
-        relative2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LightSensor.this, SendServiceToMobile.class);
-                intent.putExtra("message", "dark");
-                startService(intent);
-            }
-        });*/
 
         SensorEventListener sensorListener = new SensorEventListener() {
             @Override
@@ -76,7 +49,7 @@ public class LightSensor extends Activity {
                 if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
                     int light = (int) event.values[0];
 
-                    if (isLight && light < darknessThreshold) {
+                    if (isLight && (light < darknessThreshold) && !isDarkTaskRunning) {
                         isLight = false;
                         darkTask = new TimerTask() {
                             @Override
@@ -89,11 +62,13 @@ public class LightSensor extends Activity {
                         };
                         timer.schedule(darkTask, taskDelay);
                         isDarkTaskRunning = true;
+
+
                     } else if (!isLight && light >= darknessThreshold && isDarkTaskRunning) {
                         darkTask.cancel();
                         isLight = true;
                         isDarkTaskRunning = false;
-                    } else if (!isLight && (light > lightnessThrehold)) {
+                    } else if (!isLight && (light > lightnessThrehold) && !isLightTaskRunning) {
                         isLight = true;
                         lightTask = new TimerTask() {
                             @Override
@@ -106,6 +81,7 @@ public class LightSensor extends Activity {
                         };
                         timer.schedule(lightTask, taskDelay);
                         isLightTaskRunning = true;
+
                     } else if (isLight && light < lightnessThrehold && isLightTaskRunning) {
                         lightTask.cancel();
                         isLight = false;
